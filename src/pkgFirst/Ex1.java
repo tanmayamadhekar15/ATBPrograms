4600d3ca17dbb90d063f5eb6f4aeb6c97c63bb87
TM <tanmayamadhekar15@gmail.com>
2024-04-15 21:44:42 +0530

    First program

---
+++ src/pkgFirst/Ex1.java
@@ -0,0 +1,12 @@
+package pkgFirst;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        // This is single line comment - not compiled and executed
+        /*
+           * Author  : TM
+           * Concept : Comments (single, multi-line)
+           * This is multi line comment
+         */
+    }
+}
