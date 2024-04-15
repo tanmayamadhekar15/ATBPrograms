9f37f8a9df00a06a961ad3cc04eeded92495352f
TM <tanmayamadhekar15@gmail.com>
2024-04-15 21:58:50 +0530

    Third program added

---
+++ src/pkgFirst/Ex3.java
@@ -0,0 +1,12 @@
+package pkgFirst;
+
+/**
+ * JVM loads class
+ * finds main method
+ * execute statements one by one
+ */
+public class Ex3 {
+    public static void main(String[] args) {
+        System.out.println("JVM loads class then finds main method and execute statements one by one");
+    }
+}
