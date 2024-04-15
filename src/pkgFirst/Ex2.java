f59b3db1e8595f2ebb95f4f7fdeb6174baf11c24
TM <tanmayamadhekar15@gmail.com>
2024-04-15 21:48:16 +0530

    Second program

---
+++ src/pkgFirst/Ex2.java
@@ -0,0 +1,16 @@
+package pkgFirst;
+/* Java class follows methods then follows statements
+    println - Adds a new line like \n,  returns string
+    print - Doesn't add a new line
+
+    System is a class , out is of Print stream class, println is method of Print stream
+ */
+public class Ex2 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Welcome");
+        System.out.print("Doesn't add a new line, displays on same line");
+        System.out.println("Println adds a new line");
+
+    }
+}
